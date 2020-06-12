550ec55a76276ec15f43353aa5fd467df4bcba0e
plkujaw <jawor.jakub@gmail.com>
2020-06-12 13:10:14 +0100

    npm init and clean up codebase

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+module.exports = Phrase;
 
-// Reverses a string.
+
+// Adds 'reverse' to all strings.
 
 String.prototype.reverse = function reverse() {
   return Array.from(this).reverse().join("");
@@ -22,47 +24,3 @@ function Phrase(content) {
   this.palindrome = function palindrome() {
     return this.processedContent() === this.processedContent().reverse();  // returns result of this comparison
   }
-
-  // Makes the phrase LOUDER.
-  // this.louder = function louder() {
-  //   return this.content.toUpperCase();
-  // };
-}
-
-//
-// // Defines a TranslatedPhrase object
-// function TranslatedPhrase(content, translation) {
-//   this.content = content;
-//   this.translation = translation;
-//
-//   // Returns content processed for palindrome testing,
-//   this.processedContent = function processedContent() {
-//     return this.processor(this.translation);
-//   }
-// }
-
-
-// // Inheritance - TranslatedPhrase object inherits methods from Phrase object (Phrase object is the prototype for TranslatedPhrase object)
-// TranslatedPhrase.prototype = new Phrase();
-
-// String.prototype.palindrome = function palindrome() {
-//   return new Phrase(this).palindrome();
-// }
-
-// function reverse(string) {
-//   return Array.from(string).reverse().join("");
-// }
-
-// Add a blank method to the String object prototype that returns true if the string is empty or consists solely of whitespace (spaces, tabs, or newlines). Hint: Use a regular expression. You will need the regex syntax for the start and end of a string.
-
-// String.prototype.blank = function blank() {
-//   let whitespace = /^\s*$/g;
-//   return !!this.match(whitespace);
-// };
-
-
-// Using whichever method you prefer (direct indexing or slicing), add a last method to the Array object prototype that returns the last element of an array.
-
-Array.prototype.last = function () {
-  return this.slice(-1);
-};
